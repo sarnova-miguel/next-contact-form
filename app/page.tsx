7361b0d5f69b7632e50185fca41a12bb7857d933
Miguel Ramos <Miguel.Ramos@sarnova.com>
2026-01-02 15:12:38 -0500

    ui update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="mt-20 flex flex-col gap-5 justify-center items-center">
+    <main className="my-20 px-4 flex flex-col gap-5 justify-center items-center">
       <h1 className="text-2xl">Newsletter Sign Up Form</h1>
       <SignUpForm />
       {/* <BugReportForm /> */}
